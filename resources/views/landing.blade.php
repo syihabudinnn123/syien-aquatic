80e823d73c74ed128b016e4738ef87c2c06fc570
syihabudinnn123 <syihabudinnn@gmail.com>
2023-06-14 14:16:00 +0700

    revisi landing navbar

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -19,17 +19,17 @@
     </head>
     <body>
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
             <div class="container px-4 px-lg-5">
                 <a class="navbar-brand" href="#!">Syien Aquatic</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                        <li class="nav-item"><a class="nav-link active" href="#!">About</a></li>
                         <li class="nav-item dropdown">
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Categories</a>
+                            <a class="nav-link dropdown-toggle active" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Categories</a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 @foreach ($categories as $category)
                                     <li><a class="dropdown-item" href="{{ route('landing', ['category' => $category->name]) }}">{{ $category->name }}</a></li>
@@ -41,16 +41,24 @@
                         </li>
                     </ul>
                     <form class="d-flex">
-                        <button class="btn btn-outline-dark" type="submit">
+                        <button class="btn btn-outline-dark active bg-secondary" type="submit">
                             <i class="bi-cart-fill me-1"></i>
                             Cart
-                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                            <span class="badge bg-secondary text-white ms-1 rounded-pill">0</span>
                         </button>
-                        <a class="btn btn-outline-dark ms-1" type="" href="{{ route('login')}}">
+                        @auth
+                        <a href="{{ route('dashboard') }}" class="btn btn-outline-dark ms-1 active bg-secondary">
+                            <i class="bi-person-fill me-1"></i>
+                            Dashboard
+                        </a>
+                    @endauth
+
+                    @guest
+                        <a href="{{ route('login') }}" class="btn btn-outline-light ms-1">
                             <i class="bi-person-fill me-1"></i>
                             Login
-                            <!-- <span class="badge bg-dark text-white ms-1 rounded-pill"></span> -->
                         </a>
+                    @endguest
                     </form>
                 </div>
             </div>
@@ -181,7 +189,7 @@
           <!-- Grid column -->
           <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
             <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
-            @foreach ( $products as $prod)    
+            @foreach ( $products->take(5) as $prod)    
             <p>
               <a class="text-white" href="{{ route('landing', ['products' => $prod->name])}}">{{ $prod->name}}</a>
             </p>
